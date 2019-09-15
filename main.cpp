d86c6bfaa00c0a998ac2a6b1cc8a7d89d7dbc1b5
Tekacity <ebou.taal@protonmail.com>
2019-09-15 20:06:06 +0000

    game start

--- main.cpp
+++ main.cpp
@@ -4,22 +4,42 @@
 
 Player *P_Target;
 Enemy  *E_Target;
+int choice;
 
 
+int start_game(){
+
+
+
+    if(choice == 1){
+
+        //start game
+
+    }
+
+    else if(choice == 2){
+
+
+        return 0;
+
+    }
+
+
+    else { 
+
+        start_game();
+
+    }
+
+}
 
 int main(){
 
 
     std::cout << "1.Start  \n 2.Quit" << std::endl;
-    int choice;
-    choice = NULL;
-    std::cin >> choice;
-    if(choice == 2){
+    std::cout << "Press 1 to Start, 2 to Quit \n";
+    start_game();
 
-        return 0;
-
-    }
-    
 
 
 
