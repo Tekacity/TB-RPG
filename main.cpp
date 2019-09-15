1eadfc8479509d9e36d4487a35ac10ce058680ed
Tekacity <ebou.taal@protonmail.com>
2019-09-15 04:43:02 +0000

    start button and quit

--- main.cpp
+++ main.cpp
@@ -6,7 +6,23 @@ Player *P_Target;
 Enemy  *E_Target;
 
 
+
 int main(){
+
+
+    std::cout << "1.Start  \n 2.Quit" << std::endl;
+    int choice;
+    choice = NULL;
+    std::cin >> choice;
+    if(choice == 2){
+
+        return 0;
+
+    }
+    
+
+
+
 
 
 
