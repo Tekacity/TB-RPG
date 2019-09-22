31c18c93bccf407e0b61bb96505e1ea451741594
Tekacity <ebou.taal@protonmail.com>
2019-09-22 15:13:18 +0000

    Player and Enemy

--- main.cpp
+++ main.cpp
@@ -1,16 +1,21 @@
 #include <iostream>
+#include <iomanip>
 #include "player.h"
 #include "enemy.h"
 
-//Player *P_Target;
-//Enemy  *E_Target;
+Player *P_Target;
+Enemy  *E_Target;
 int choice;
+int TurnCount;
 
 int start_game();
 
 int main(){
 
-
+    Player Char;
+    Enemy  Foe;
+    E_Target = &Foe;
+    P_Target = &Char;
     std::cout << "1.Start  \n 2.Quit" << std::endl;
     //std::cout << "Press 1 to Start, 2 to Quit \n";
     //start_game();
@@ -22,7 +27,6 @@ int main(){
     return 0;
 
 }
-
 int start_game(){
 
     std::cout << "Press 1 to Start, 2 to Quit \n";
@@ -47,3 +51,39 @@ int start_game(){
 
     return choice;
 }
+
+void Battle_Start(){
+
+    TurnCount = 1;
+    choice = NULL;
+
+}
+void Initiate_PTurn(){
+
+    TurnCount++; 
+    std::cout << "1. Attack" << std::setw(5) << "2.Defend \n";
+    std::cin >> choice;
+    if(choice == 1){ 
+    P_Target->attack(E_Target);
+
+    }
+
+    else{
+
+        std::cout << "Defending... \n";
+
+        
+    }
+
+}
+
+void Enemy_Turn(){
+
+    E_Target->attack(P_Target);
+
+
+
+
+}
+
+
