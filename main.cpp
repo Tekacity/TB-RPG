9db6e2ae369f8002334c56db4ce410fd935d34e4
Tekk <48891590+Tekacity@users.noreply.github.com>
2019-11-16 20:55:48 +0000

    TURN BASED SYSTEM FIXED

--- main.cpp
+++ main.cpp
@@ -19,9 +19,8 @@ int main(){
     Enemy  Foe;
     E_Target = &Foe;
     P_Target = &Char;
-    start_game();
-    Char.set_stats("Hero", 200, 100, 0);
-    Foe.set_stats("Monster", 200, 100, 0);
+    Char.set_stats("Hero", 300, 100, 0);
+    Foe.set_stats("Monster", 300, 100, 0);
     start_game();
 
 
@@ -34,23 +33,23 @@ int main(){
 }
 int start_game(){
 
-    std::cout << "Press 1 to Start, 2 to Quit \n";
+    std::cout << "Press 1 to Start" << std::setw(20) <<"2 to Quit \n";
     std::cin >> choice;
 
     if(choice == 1){
 
-        std::cout << "BATTLE START! '\n'";
+        std::cout << "BATTLE START! \n";
         Battle_Start();
     }
 
     else if(choice == 2){
 
         return 1;
-        
+
     }
 
 
-    else { 
+    else {
 
         start_game();
     }
@@ -67,35 +66,61 @@ void Battle_Start(){
 }
 void PlayerTurn(){
 
-    std::cout << "1. Attack" << std::setw(5) << "2.Defend \n";
-    std::cin >> choice;
-    if(choice == 1 && E_Target->HP > 0){ 
-    P_Target->attack(E_Target);
-    TurnCount++;
+    if(E_Target->HP > 0){
     
+    std::cout << "1. Attack" << std::setw(15) << "2.Defend" << std::setw(15) <<"3.Skip Turn\n";
+    std::cin >> choice;
+    if(choice == 1 && E_Target->HP > 0){
 
+    P_Target->attack(E_Target);
+    EnemyTurn();
+    }
+    else if(choice == 2){
+        std::cout << "Defending...";
+        TurnCount++;
+        EnemyTurn();
+
+    }
+
+    else if(choice == 3){
+       std::cout << "Turn Skipped \n" ;
+       TurnCount++;
+        EnemyTurn();
     }
 
     else{
 
-        std::cout << "Defending... \n";
-   
+        PlayerTurn();
+    }
+
+
+
+    }
+
+    else if(E_Target->HP <= 0){
+
+        std::cout << E_Target->name << " defeated!";
     }
 
 }
 
 void EnemyTurn(){
-    if(P_Target->HP <= 0) {
 
-        std::cout <<"BATTE OVER '\n'";
-    }
-
-    else if(P_Target->HP > 0){
+    if(P_Target->HP > 0){
 
         E_Target->attack(P_Target);
-        PlayerTurn();
         TurnCount++;
+        PlayerTurn();
     }
+    
+    
+    else if(P_Target->HP <= 0) {
+
+        std::cout << P_Target->name << " has been defeated... \n";
+        std::cout <<"GAME OVER '\n'";
+    }
+
+    
 
     else{
         std::cout<<"Nothing Happened'\n'";
@@ -104,7 +129,3 @@ void EnemyTurn(){
 
 
 }
-
-
-
-
